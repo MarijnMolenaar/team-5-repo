2610d39d5144a0854eeeeba66866f50629e6a086
Tessa Welling <tessafwelling@gmail.com>
2022-04-01 11:28:03 +0200

    test

---
+++ public/js/filterscript.js
@@ -0,0 +1,22 @@
+// De 'REST Countries' API wordt ingeladen in het document
+document.addEventListener('DOMContentLoaded', () => {
+    const CountryDropdown = document.getElementById('country');
+
+    fetch('https://restcountries.com/v2/all').then(res => {
+        return res.json();
+    }).then(countries => {
+        let output = "";
+
+        // Er wordt door elk land in de API heen 'geloopt', waarmee elke naam van het land uit het object gehaald wordt en wordt toegevoegd aan een output variabele
+        countries.forEach(country => {
+            output += `<option>${country.name}</option>`;
+        });
+        
+        // Vervolgens wordt deze output toegevoegd aan het select element, welke eerder uit de EJS pagina is opgehaald
+        CountryDropdown.innerHTML = output;
+    }).catch(err => {
+        // Bij sprake van een error wordt deze error naar de console gelogd
+        console.log(err);
+    })
+});
+// Bron gebruikt: https://www.youtube.com/watch?v=iw4lvZGBuvA&ab_channel=OstonCodeCypher
---
+++ public/js/homescript.js
@@ -0,0 +1,7 @@
+const h2check = document.getElementById("h2check");
+const homebutton = document.getElementById("homemessage");
+
+// Wanneer op de button wordt geklikt wordt de profile-pagina geladen
+homebutton.addEventListener("click", () => {
+    window.location.href = 'profile';
+})
