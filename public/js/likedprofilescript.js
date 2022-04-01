4d71d2ad70a9558731407185f100e7cc1e2a8535
Joost Verweijen <joosiiiv@gmail.com>
2022-04-01 14:57:31 +0200

    Liked Profiles Detail Pages
    When clicking the arrow button next to a liked profile you get redirected to a page where you can view the profile.

---
+++ public/js/likedprofilescript.js
@@ -0,0 +1,19 @@
+const divhuman = document.getElementById("humanprofile");
+const divanimal = document.getElementById("animalprofile");
+
+// De functie van het menselijke profiel wordt uitgevoerd bij het opstarten van de pagina, aangezien deze laten zien moet worden
+HumanProfile();
+
+// Binnen deze functie wordt het menselijke profiel weergegeven en wordt het dieren profiel verborgen door de display-style naar none te veranderen
+function HumanProfile() {
+    divhuman.style.display = "block";
+    divanimal.style.display = "none";
+}
+
+// Hier gebeurt dit ook, maar dan vice versa
+function AnimalProfile() {
+    divhuman.style.display = "none";
+    divanimal.style.display = "block";
+}
+
+document.getElementById("likespage").classList.add("active");
