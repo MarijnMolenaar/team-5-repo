3edd4b714c0084da6c1b7f1d81dcf883f824055c
Joost Verweijen <joosiiiv@gmail.com>
2022-04-03 23:33:17 +0200

    Mongoose Added
    Mongoose wordt toegevoegd (Is gedaan door Whitney maar bij haar ging er iets fout tijdens het pushen)

---
+++ model/matches.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const { Schema } = mongoose;
+//structuur van  de database 
+
+const matchesSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    name: String,
+    country: String,
+    bio: String,
+    interests: [String],
+    name_a : String,
+    age_a : Number,
+    type_a : String,
+    breed_a : String,
+    bio_a : String,
+    liked : String
+})
+module.exports = mongoose.model("Match", matchesSchema);
